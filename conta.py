7de5cf71777d13be29940ccafd5b338e7194716d
Thiago-Souz <125496318+Thiago-Souz@users.noreply.github.com>
2023-06-19 20:50:31 -0300

    Update conta.py

--- conta.py
+++ conta.py
@@ -41,3 +41,6 @@ class Conta:
     def limite(self, limite):
         self.__limite = limite
 
+    @staticmethod
+    def codigos_bancos():
+    return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
